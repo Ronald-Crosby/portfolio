0c3948bb7e570e4e2127dafe01edbfb639b5c071
Olly Robinson <olly@parall.ax>
2019-10-08 17:34:56 +0100

    typography and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      blue: '#283CDA',
+      purple: '#6400D7',
+      pink: '#FFC7DB',
+      lightBlue: '#00DCFF',
+      white: 'ffffff'
+    }
   },
   variants: {},
   plugins: [],
