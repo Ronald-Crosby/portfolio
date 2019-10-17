5d81fb537fc9beb8619d2ed99b7eec738116985f
Olly Robinson <olly@parall.ax>
2019-10-17 17:57:08 +0100

    parallax scroll effect

--- public/main.js
+++ public/main.js
@@ -14,8 +14,6 @@ function updateBackground() {
 
         if(windowTopPosition + 500 >= sectionTop) {
             const dataColor = $section.getAttribute('data-color')
-            console.log(dataColor)
-
             mainTag.className = "transitionBackground overflow-hidden " + dataColor
         }
 
@@ -24,3 +22,32 @@ function updateBackground() {
 
 document.addEventListener('scroll', updateBackground)
 updateBackground()
+
+// parallax effect on SVGs
+
+const $$scrollEffectImages = document.querySelectorAll('.scroll-effect-image')
+console.log($$scrollEffectImages)
+const $$SVGTags = document.querySelectorAll(`[data-scroll]`)
+console.log($$SVGTags)
+const windowHeight = window.innerHeight
+
+document.addEventListener('scroll', () => {
+    const scrolledDistance = window.pageYOffset
+    const viewportMiddle = window.pageYOffset + (window.innerHeight / 2)
+
+    $$scrollEffectImages.forEach($image => {
+        const imageMiddle = $image.offsetTop + ($image.offsetHeight / 2)
+        const distanceToImageMiddle = viewportMiddle - imageMiddle
+
+        $$SVGTags.forEach($SVG => {
+            const speed = parseFloat($SVG.getAttribute('data-scroll'))
+            if($SVG.classList.contains('js-translateY')) {
+                $SVG.style.transform = `translateY(${distanceToImageMiddle * speed}px)`
+            } else if ($SVG.classList.contains('js-translateX')) {
+                $SVG.style.transform = `translateX(${distanceToImageMiddle * speed}px)`
+            }
+        })
+
+
+    })
+})
